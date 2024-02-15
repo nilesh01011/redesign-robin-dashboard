38fbadff098b1ba2d694a6a2032e9963841ef7fb
unknown <Admin@FDLW190004.corp.mahindra.com>
2024-02-15 16:30:09 +0530

    date components added

--- src/components/date/Date-Range-picker/MyRangePicker.js
+++ src/components/date/Date-Range-picker/MyRangePicker.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { DatePicker } from "antd";
 import "./styles.scss";
+import {useSelector} from "react-redux"
 
 const { RangePicker } = DatePicker;
 
 const MyRangePicker = ({ onChange }) => {
+  const theme = useSelector((state) => state.theme);
   const handleRangePickerChange = (dates, dateStrings) => {
     // console.log("Selected Range:", dates);
     console.log("Selected Date Strings:", dateStrings);
--- src/components/date/Date-picker/MyDatePicker.js
+++ src/components/date/Date-picker/MyDatePicker.js
@@ -2,9 +2,11 @@
 import React from "react";
 import { DatePicker } from "antd";
 import "./styles.scss";
+import {useSelector} from "react-redux"
 
 
 const MyDatePicker = ({ onChange }) => {
+  const theme = useSelector((state) => state.theme);
   return (
     <DatePicker
       className="cal1"
